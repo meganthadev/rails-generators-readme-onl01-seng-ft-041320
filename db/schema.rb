b0afe64d92392b4b166f3faa0c8924beb56b7bb6
Megan Horsfield <meganthadev@gmail.com>
2020-06-28 03:43:22 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200626055439) do
+ActiveRecord::Schema.define(version: 20200628030159) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
     t.string   "payment_status"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "authors", force: :cascade do |t|
